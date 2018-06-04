0b74f9777cfae5bf25755e6bd06e74777bc0a0a6
Matthew Walcher <mwalcher@live.ca>
2018-06-03 21:50:55 -0400

    removed default content and unneeded files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="app">
+                <h1>React Weather App</h1>
+            </div>
+        );
+    }
 }
 
 export default App;
